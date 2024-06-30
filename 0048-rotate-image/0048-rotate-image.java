9f385e75dbb03ccde042ebeaadc5de8bf3905a00
Shreya Rai <102602566+Shreya2114@users.noreply.github.com>
2024-06-30 12:55:07 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (27.21%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+//         transpose
+        for(int i =0; i<m; i++){
+            for(int j =0; j<i; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+//         Reverse
+        for(int i =0; i<m; i++){
+            int a =0, b= m-1;
+            while(a<b){
+                int temp = matrix[i][a];
+                matrix[i][a] = matrix[i][b];
+                matrix[i][b] = temp;
+                a++;
+                b--;
+            }
+        }
+        
+    }
+}
